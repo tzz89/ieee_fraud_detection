361fb231d926e2c420a38c792ddd42ab6cf21f49
George <teozz89@gmail.com>
2021-05-09 20:07:10 +0800

    added read_schema utils and memory reducer class

--- src/utils/data_transform/data_transform.py
+++ src/utils/data_transform/data_transform.py
@@ -1,70 +1,70 @@
-import math
 import gc
 import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
 
 
-class DataMemoryReducer():
+class DataMemoryReducer(BaseEstimator, TransformerMixin):
     '''
-    This module attempts to reduce 64 bit datatypes to 32 bit
+    This module attempts to reduce 64 bit datatypes to 32 bit during EDA with pandas so it will fit memory. It will also save the schema to given location
     '''
+
     def __init__(self):
         self.float_columns_to_reduce = []
-        self.int_columns_to_reduce  = []
+        self.int_columns_to_reduce = []
         self.__float_min = -3.4028235E+38
-        self.__float_max =  3.4028235e+38
+        self.__float_max = 3.4028235e+38
         self.__int_min = -2147483648
         self.__int_max = 2147483647
-        
-            
+
     def fit(self, pandas_df):
+        self.data_schema = pandas_df.dtypes.to_dict()
         float_df = pandas_df.select_dtypes(include='float64')
-        if len(float_df.columns) == 0:
+        float_columns = float_df.columns
+        if len(float_columns) == 0:
             print("None of the float columns can be changed")
-            
-        else: 
-            #if all the columns are within range, change all
-            if  (np.nanmax(float_df.values) <= self.__float_max) and (np.nanmin(float_df.values) >= self.__float_min):
-                self.float_columns_to_reduce = float_df.columns
+
+        else:
+            # if all the columns are within range, change all
+            if (np.nanmax(float_df.values) <= self.__float_max) and (np.nanmin(float_df.values) >= self.__float_min):
+                self.float_columns_to_reduce = float_columns
 
             else:
-                self.float_columns_to_reduce = [] #clear any residuals
-                for float_column in float_df.columns:
-                    if ((np.nanmax(float_df[float_column].values) <= self.__float_max) and 
+                self.float_columns_to_reduce = []  # clear any residuals
+                for float_column in float_columns:
+                    if ((np.nanmax(float_df[float_column].values) <= self.__float_max) and
                        (np.nanmin(float_df[float_column].values) >= self.__float_min)):
                         self.float_columns_to_reduce.append(float_column)
-                     
+
         del float_df
         gc.collect()
-        
-        
+
         int_df = pandas_df.select_dtypes(include='int64')
-        if len(int_df.columns) == 0:
+        int_columns = int_df.columns
+        if len(int_columns) == 0:
             print("None of the int columns can be changed")
-        else:    
-            #if all the columns are within range, change all
-            if  (np.nanmax(int_df.values) <= self.__int_max) and (np.nanmin(int_df.values) >= self.__int_min):
+        else:
+            # if all the columns are within range, change all
+            if (np.nanmax(int_df.values) <= self.__int_max) and (np.nanmin(int_df.values) >= self.__int_min):
                 self.int_columns_to_reduce = int_df.columns
-            #check each column one by one
+
+            # check each column one by one
             else:
                 self.int_columns_to_reduce = []
-                for int_column in int_df.columns:
-                    if ((np.nanmax(int_df[int_column].values) <= self.__int_max) and 
+                for int_column in int_columns:
+                    if ((np.nanmax(int_df[int_column].values) <= self.__int_max) and
                        (np.nanmin(int_df[int_column].values) >= self.__int_min)):
                         self.int_columns_to_reduce.append(int_column)
-            
-            
+
         del int_df
         gc.collect()
+        return self
 
     def transform(self, pandas_df):
         temp_df = pandas_df.copy()
-        temp_df[self.float_columns_to_reduce]= temp_df[self.float_columns_to_reduce].astype('float32')
+        temp_df[self.float_columns_to_reduce] = temp_df[self.float_columns_to_reduce].astype(
+            'float32')
         gc.collect()
-        temp_df[self.int_columns_to_reduce] = temp_df[self.int_columns_to_reduce].astype('int32')
+        temp_df[self.int_columns_to_reduce] = temp_df[self.int_columns_to_reduce].astype(
+            'int32')
         gc.collect()
         return temp_df
-    
-    def fit_transform(self, pandas_df):
-        self.fit(pandas_df)
-        return self.transform(pandas_df)
-        
--- src/utils/misc.py
+++ src/utils/misc.py
@@ -1,2 +1,33 @@
+import pickle
+import os
+
+
 def read_config_file():
     pass
+
+
+def save_schema(dataframe, path="", file_name="schema.pkl"):
+    '''
+    This function will save the schema (dictionary) of a the dataframe in pickle format
+    dataframe: pandas dataframe
+    path: folder to store the schema file
+    filename: name of the schema file, will automatically add .pkl if not added
+
+    Example usage
+
+    file_name = "raw_schema"
+    save_schema(pandas_dataframe ,path="", file_name=file_name)
+
+    with open(file_name+'.pkl', 'rb') as handles:
+        loaded_schema = pickle.load(handles)
+    print(loaded_schema)
+    >> {'TransactionID': dtype('int64'), 'isFraud': dtype('int64'), 
+        'TransactionDT': dtype('int64'), 
+        'card1': dtype('int64'), 'V330': dtype('float64')}
+    '''
+    schema = dataframe.dtypes.to_dict()
+    if file_name[-4:] != ".pkl":
+        file_name = file_name + ".pkl"
+    file_location = os.path.join(path, file_name)
+    with open(file_location, "wb") as handles:
+        pickle.dump(schema, handles, protocol=pickle.HIGHEST_PROTOCOL)
